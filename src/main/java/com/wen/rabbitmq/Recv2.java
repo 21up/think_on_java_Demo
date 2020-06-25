9b5fd8c66327cfd8ea41213c5970cc06e2f72056
21up <1109287523@qq.com>
2020-06-25 09:48:35 +0800

    rabbitmq:手动ack查看消息状态从unacked被消费掉

--- src/main/java/com/wen/rabbitmq/Recv2.java
+++ src/main/java/com/wen/rabbitmq/Recv2.java
@@ -10,14 +10,14 @@ public class Recv2 {
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Connection connction = ConnectionUtil.getConnction();
-        Channel channel = connction.createChannel();
+        final Channel channel = connction.createChannel();
         channel.queueDeclare(QUEUE_NAME,false,false,false,null);
         DefaultConsumer consumer = new DefaultConsumer(channel){
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                String msg = new String(body);
-               int i=1/0;
                 System.out.println("[x] received:"+msg+"!");
+                channel.basicAck(envelope.getDeliveryTag(),false);
             }
         };
         channel.basicConsume(QUEUE_NAME,false,consumer);
