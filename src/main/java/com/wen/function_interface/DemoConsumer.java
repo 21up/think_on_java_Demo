a3a54a4e9b98d3b6fe14e6cb1a8c4afd907a8553
21up <1109287523@qq.com>
2020-07-04 20:09:06 +0800

    函数式接口：Consumer接口andThen方法实现格式化打印信息

---
+++ src/main/java/com/wen/function_interface/DemoConsumer.java
@@ -0,0 +1,18 @@
+package com.wen.function_interface;
+
+import javax.naming.Name;
+import java.util.function.Consumer;
+
+public class DemoConsumer {
+
+    public static void main(String[] args) {
+        String[] array = { "迪丽热巴,女", "古力娜扎,女", "马尔扎哈,男" };
+        printInSomeway(s-> System.out.print("姓名："+s.split(",")[0]),
+                s-> System.out.println(",性别："+s.split(",")[1]),array);
+    }
+    public static void printInSomeway(Consumer<String> name,Consumer<String> sex,String [] array){
+        for (int i=0;i<3;i++){
+            name.andThen(sex).accept(array[i]);
+        }
+    }
+}
