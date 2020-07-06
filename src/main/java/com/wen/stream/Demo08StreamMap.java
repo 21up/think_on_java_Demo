c9c616f3c22705b5175cc0d0ee941b4cfb735bdf
21up <1109287523@qq.com>
2020-07-06 10:06:49 +0800

    stream流:常用方法-limit犯法

--- src/main/java/com/wen/stream/Demo08StreamMap.java
+++ src/main/java/com/wen/stream/Demo08StreamMap.java
@@ -7,6 +7,6 @@ public class Demo08StreamMap {
     public static void main(String[] args) {
         Stream<String> original=Stream.of("10","12","18");
         Stream<Integer> result=original.map(str->Integer.parseInt(str));
-        System.out.println(result.count());
+        System.out.println(result.limit(2).count());
     }
 }
