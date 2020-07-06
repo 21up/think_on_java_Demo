db76fb8b142a1224c69197787c9c0a34fca8d08b
21up <1109287523@qq.com>
2020-07-06 10:11:14 +0800

    stream流:常用方法-skip方法

--- src/main/java/com/wen/stream/Demo08StreamMap.java
+++ src/main/java/com/wen/stream/Demo08StreamMap.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class Demo08StreamMap {
     public static void main(String[] args) {
         Stream<String> original=Stream.of("10","12","18");
-        Stream<Integer> result=original.map(str->Integer.parseInt(str));
-        System.out.println(result.limit(2).count());
+        Stream<String> skip = original.skip(2);
+        System.out.println(skip.count());
     }
 }
