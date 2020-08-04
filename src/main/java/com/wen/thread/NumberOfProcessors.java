543b3d2c84fe45424b81f3dcf9e0b1e6329e42cf
21up <1109287523@qq.com>
2020-08-04 20:12:57 +0800

    并发底层原理-查出当前机器上处理器的数量

---
+++ src/main/java/com/wen/thread/NumberOfProcessors.java
@@ -0,0 +1,10 @@
+package com.wen.thread;
+
+/**
+ * Created by 21up on 2020/8/4
+ */
+public class NumberOfProcessors {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
